e2c236b73a3496f4feba857695a3a43207fec818
Dante Castillo <dante.castillo@inallmedia.com>
2020-04-07 17:54:09 -0600

    i18 n complete

---
+++ components/Footer.vue
@@ -0,0 +1,375 @@
+<template>
+<footer class="footer container-fluid">
+      <address class="container">
+        <span class="col-xs-12 col-sm-6 col-md-4"
+          ><span class="address-title h4">{{$t('footer.title')}}</span>
+          {{$t('footer.address1')}}<br />{{$t('footer.address2')}}<br />{{$t('footer.address3')}}</span
+        >
+        <span class="col-xs-12 col-sm-6 col-md-4"
+          > {{$t('footer.tel')}}<br /> {{$t('footer.fax')}}</span
+        >
+        <span class="col-xs-12 col-sm-6 col-md-4">
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/welcome?set_language=en"
+            >{{$t('footer.link1')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/service?set_language=en"
+            >{{$t('footer.link2')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/dynajet/career?set_language=en"
+            >{{$t('footer.link3')}}</a
+          ></span
+        >
+      </address>
+      <div class="container">
+        <div class="col-xs-12 col-sm-6 col-md-4">
+          <h4>{{$t('footer.applicationTitle')}}</h4>
+
+          <a
+            title="applicationTitle"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/application-reports/chewing-gum-removal?set_language=en"
+            >{{$t('footer.application1')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/application-reports/graffiti-removal?set_language=en"
+            >{{$t('footer.application2')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/application-reports/cleaning-train-stations-and-multi-storey-car-parks?set_language=en"
+            >{{$t('footer.application3')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/application-reports/concrete-restoration?set_language=en"
+            >{{$t('footer.application4')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/application-reports/concrete-removal-on-a-large-construction-site?set_language=en"
+            >{{$t('footer.application5')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/application-reports/cleaning-shuttering?set_language=en"
+            >{{$t('footer.application6')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/application-reports/cleaning-construction-machinery-and-installations?set_language=en"
+            >{{$t('footer.application7')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/application-reports/facade-renovation?set_language=en"
+            >{{$t('footer.application8')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/application-reports/paint-removal?set_language=en"
+            >{{$t('footer.application9')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/application-reports/cleaning-drums-tanks-and-containers?set_language=en"
+            >{{$t('footer.application10')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/application-reports/cleaning-drums-tanks-and-containers?set_language=en"
+            >{{$t('footer.application11')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/application-reports/cleaning-drums-tanks-and-containers?set_language=en"
+            >{{$t('footer.application12')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/application-reports/removing-bark-from-trees?set_language=en"
+            >{{$t('footer.application13')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/application-reports/weed-control?set_language=en"
+            >{{$t('footer.application14')}}</a
+          >
+
+          <h4>{{$t('footer.branchesTitle')}}</h4>
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/sector-specific-solutions/construction"
+            >{{$t('footer.branch1')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/sector-specific-solutions/renovation"
+            >{{$t('footer.branch2')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/sector-specific-solutions/service-providers"
+            >{{$t('footer.branch3')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/sector-specific-solutions/local-authorities"
+            >{{$t('footer.branch4')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/sector-specific-solutions/shipyards-and-offshore"
+            >{{$t('footer.branch5')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/sector-specific-solutions/agriculture-and-forestry"
+            >{{$t('footer.branch6')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/sector-specific-solutions/agriculture-and-forestry"
+            >{{$t('footer.branch7')}}</a
+          >
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4">
+          <h4>{{$t('footer.cleanersTitle')}}</h4>
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/hochdruckreiniger#b_start=0"
+            >{{$t('footer.cleaner1')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/hochdruckreiniger/used-high-pressure-cleaners"
+            >{{$t('footer.cleaner2')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/datasheets-catalogues/dynajet-general-catalogue-edition-no.-6.pdf"
+            >{{$t('footer.cleaner3')}}</a
+          >
+
+          <h4>{{$t('footer.accessoriesTitle')}}</h4>
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/zubehoershop/find-an-accessory#b_start=0"
+            >{{$t('footer.accessory1')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/zubehoershop/find-an-accessory#b_start=0&c5=zubehoersets"
+            >{{$t('footer.accessory2')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/zubehoershop/find-an-accessory#b_start=0&c5=duesen"
+            >{{$t('footer.accessory3')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/zubehoershop/find-an-accessory#b_start=0&c5=lanzen-pistolen"
+            >{{$t('footer.accessory4')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/zubehoershop/find-an-accessory#b_start=0&c5=lanzen-pistolen"
+            >{{$t('footer.accessory5')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/zubehoershop/find-an-accessory#b_start=0&c5=schutzausruestung"
+            >{{$t('footer.accessory6')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/zubehoershop/find-an-accessory#b_start=0&c5=schlaeuche"
+            >{{$t('footer.accessory7')}}</a
+          >
+
+          <h4>{{$t('footer.knowledgeTitle')}}</h4>
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/videos/videos?set_language=en"
+            >{{$t('footer.knowledge1')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/application-reports/anwendungsberichte?set_language=en"
+            >{{$t('footer.knowledge2')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/expert-tips/dynajet-advisor?set_language=en"
+            >{{$t('footer.knowledge3')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/info-center/ask-our-experts/fragen-sie-unsere-experten?set_language=en"
+            >{{$t('footer.knowledge4')}}</a
+          >
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4">
+          <h4>{{$t('footer.dynaJetTitle')}}</h4>
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/dynajet/company/company?set_language=en"
+            >{{$t('footer.dynaJet1')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/dynajet/company/locations/dynajet-germany?set_language=en"
+            >{{$t('footer.dynaJet2')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/dynajet/company/managment-team?set_language=en"
+            >{{$t('footer.dynaJet3')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/dynajet/career/current-vacancies"
+            >{{$t('footer.dynaJet4')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/dynajet/career?set_language=en"
+            >{{$t('footer.dynaJet5')}}</a
+          >
+
+          <h4>{{$t('footer.contactTitle')}}</h4>
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/kontakt"
+            >{{$t('footer.contact1')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/impressum?set_language=en"
+            >{{$t('footer.contact2')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/agb?set_language=en"
+            >{{$t('footer.contact3')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.dyna-jet.com/rechtlicher-hinweis?set_language=en"
+            >{{$t('footer.contact4')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://de-de.facebook.com/Dynajet-GmbH-207440125973475/"
+            >{{$t('footer.contact5')}}</a
+          >
+
+          <a
+            title="Link in neuem Fenster öffnen"
+            target="_blank"
+            href="https://www.youtube.com/channel/UCQ7N367vjoIz-oXuHk0Zfag"
+            >{{$t('footer.contact6')}}</a
+          >
+        </div>
+      </div>
+    </footer>
+
+</template>
