4e0bd02e285055a89e1357351cd68669a2356594
dantedubon <dante.dubon@gmail.com>
2020-04-08 14:41:25 -0600

    form completed

---
+++ components/Form.vue
@@ -0,0 +1,151 @@
+<template>
+ <form id="contact-form" class="form-horizontal">
+          <div class="col-xs-12 col-md-6">
+            <div class="form-group">
+              <label for="company" class="col-sm-3 control-label"
+                >Empresa<sup class="text-req"> * </sup></label
+              >
+              <div class="col-sm-9" data-children-count="1">
+                <input
+                  id="company"
+                  name="company"
+                  type="text"
+                  value=""
+                  required="required"
+                  class="form-control"
+                  data-kwimpalastatus="alive"
+                  data-kwimpalaid="1586376103791-0"
+                />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="firstname" class="col-sm-3 control-label"
+                >Nombre<sup class="text-req"> * </sup></label
+              >
+              <div class="col-sm-9" data-children-count="1">
+                <input
+                  id="firstname"
+                  name="firstname"
+                  type="text"
+                  value=""
+                  required="required"
+                  class="form-control"
+                  data-kwimpalastatus="alive"
+                  data-kwimpalaid="1586376103791-1"
+                />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="lastname" class="col-sm-3 control-label"
+                >Apellido<sup class="text-req"> * </sup></label
+              >
+              <div class="col-sm-9" data-children-count="1">
+                <input
+                  id="lastname"
+                  name="lastname"
+                  type="text"
+                  value=""
+                  required="required"
+                  class="form-control"
+                  data-kwimpalastatus="alive"
+                  data-kwimpalaid="1586376103791-2"
+                />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="city" class="col-sm-3 control-label"
+                >Ciudad<sup class="text-req"> * </sup></label
+              >
+              <div class="col-sm-9" data-children-count="1">
+                <input
+                  id="city"
+                  name="city"
+                  type="text"
+                  value=""
+                  required="required"
+                  class="form-control"
+                  data-kwimpalastatus="alive"
+                  data-kwimpalaid="1586376103791-3"
+                />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="country" class="col-sm-3 control-label"
+                >País<sup class="text-req"> * </sup></label
+              >
+              <div class="col-sm-9" data-children-count="1">
+                <input
+                  id="country"
+                  name="country"
+                  type="text"
+                  value=""
+                  required="required"
+                  class="form-control"
+                  data-kwimpalastatus="alive"
+                  data-kwimpalaid="1586376103791-4"
+                />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="user-email" class="col-sm-3 control-label"
+                >E-mail<sup class="text-req"> * </sup></label
+              >
+              <div class="col-sm-9" data-children-count="1">
+                <input
+                  id="user-email"
+                  name="user-email"
+                  type="text"
+                  value=""
+                  required="required"
+                  class="form-control"
+                  data-kwimpalastatus="alive"
+                  data-kwimpalaid="1586376103791-5"
+                />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="phone" class="col-sm-3 control-label"
+                >Teléfono<sup class="text-req"> * </sup></label
+              >
+              <div class="col-sm-9" data-children-count="1">
+                <input
+                  id="phone"
+                  name="phone"
+                  type="text"
+                  value=""
+                  required="required"
+                  class="form-control"
+                  data-kwimpalastatus="alive"
+                  data-kwimpalaid="1586376103791-6"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-md-6">
+            <div class="form-group" data-children-count="1">
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Para consultas de servicio técnico"
+                indique=""
+                el=""
+                número=""
+                de=""
+                serie=""
+                su=""
+                máquina.=""
+                rows="3"
+                required="required"
+                class="form-control"
+              ></textarea>
+            </div>
+          </div>
+        </form>
+</template>
--- pages/_lang/index.vue
+++ pages/_lang/index.vue
@@ -519,159 +519,20 @@ margin-top: 0px; margin-bottom:0px; margin-left: 0px; margin-right: 20px"
         <p>
           {{ $t('kontakt.content') }}
         </p>
-        <form id="contact-form" class="form-horizontal">
-          <div class="form-group">
-            <div class="col-xs-12 col-md-6">
-              <label for="company" class="col-sm-3 control-label"
-                >Empresa<sup class="text-req"> * </sup></label
-              >
-              <div class="col-sm-9" data-children-count="1">
-                <input
-                  id="company"
-                  name="company"
-                  type="text"
-                  value=""
-                  required="required"
-                  class="form-control"
-                  data-kwimpalastatus="alive"
-                  data-kwimpalaid="1586376103791-0"
-                />
-              </div>
-            </div>
-          </div>
-
-          <div class="form-group">
-            <div class="col-xs-12 col-md-6">
-              <label for="firstname" class="col-sm-3 control-label"
-                >Nombre<sup class="text-req"> * </sup></label
-              >
-              <div class="col-sm-9" data-children-count="1">
-                <input
-                  id="firstname"
-                  name="firstname"
-                  type="text"
-                  value=""
-                  required="required"
-                  class="form-control"
-                  data-kwimpalastatus="alive"
-                  data-kwimpalaid="1586376103791-1"
-                />
-              </div>
-            </div>
-          </div>
-
-          <div class="form-group">
-            <div class="col-xs-12 col-md-6">
-              <label for="lastname" class="col-sm-3 control-label"
-                >Apellido<sup class="text-req"> * </sup></label
-              >
-              <div class="col-sm-9" data-children-count="1">
-                <input
-                  id="lastname"
-                  name="lastname"
-                  type="text"
-                  value=""
-                  required="required"
-                  class="form-control"
-                  data-kwimpalastatus="alive"
-                  data-kwimpalaid="1586376103791-2"
-                />
-              </div>
-            </div>
-          </div>
-
-          <div class="form-group">
-            <div class="col-xs-12 col-md-6">
-              <label for="city" class="col-sm-3 control-label"
-                >Ciudad<sup class="text-req"> * </sup></label
-              >
-              <div class="col-sm-9" data-children-count="1">
-                <input
-                  id="city"
-                  name="city"
-                  type="text"
-                  value=""
-                  required="required"
-                  class="form-control"
-                  data-kwimpalastatus="alive"
-                  data-kwimpalaid="1586376103791-3"
-                />
-              </div>
-            </div>
-          </div>
-
-          <div class="form-group">
-            <div class="col-xs-12 col-md-6">
-              <label for="country" class="col-sm-3 control-label"
-                >País<sup class="text-req"> * </sup></label
-              >
-              <div class="col-sm-9" data-children-count="1">
-                <input
-                  id="country"
-                  name="country"
-                  type="text"
-                  value=""
-                  required="required"
-                  class="form-control"
-                  data-kwimpalastatus="alive"
-                  data-kwimpalaid="1586376103791-4"
-                />
-              </div>
-            </div>
-          </div>
-
-          <div class="form-group">
-            <div class="col-xs-12 col-md-6">
-              <label for="user-email" class="col-sm-3 control-label"
-                >E-mail<sup class="text-req"> * </sup></label
-              >
-              <div class="col-sm-9" data-children-count="1">
-                <input
-                  id="user-email"
-                  name="user-email"
-                  type="text"
-                  value=""
-                  required="required"
-                  class="form-control"
-                  data-kwimpalastatus="alive"
-                  data-kwimpalaid="1586376103791-5"
-                />
-              </div>
-            </div>
-          </div>
-
-          <div class="form-group">
-            <div class="col-xs-12 col-md-6">
-              <label for="phone" class="col-sm-3 control-label"
-                >Teléfono<sup class="text-req"> * </sup></label
-              >
-              <div class="col-sm-9" data-children-count="1">
-                <input
-                  id="phone"
-                  name="phone"
-                  type="text"
-                  value=""
-                  required="required"
-                  class="form-control"
-                  data-kwimpalastatus="alive"
-                  data-kwimpalaid="1586376103791-6"
-                />
-              </div>
-            </div>
-          </div>
-        </form>
+        <Form/>
       </div>
+     
     </section>
     <!-- Ende - Section Kontakt -->
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Form from '~/components/Form.vue'
 
 export default {
   components: {
-    Logo
+    Form
   }
 }
 </script>
